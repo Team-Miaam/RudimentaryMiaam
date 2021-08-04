52dadb9a68dbf777b747e8269d86bb29286a311f
Noman5237 <anonyman637@gmail.com>
2021-08-04 10:40:07 +0600

    #5 barebone entity class TODO: load resources

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import GameManager from './src/manager/gameManager.js';
 import SceneManager from './src/manager/sceneManager.js';
 import Scene from './src/core/scene.js';
+import Entity from './src/core/entity.js';
 
-export { GameManager, SceneManager, Scene };
+export { GameManager, SceneManager, Scene, Entity };
