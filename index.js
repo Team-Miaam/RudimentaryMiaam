dca2e030b6f5ac1246193a9d0aeaf554d993c150
Noman5237 <anonyman637@gmail.com>
2021-08-04 10:20:59 +0600

    #4 Scene exported

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import GameManager from './src/manager/gameManager.js';
 import SceneManager from './src/manager/sceneManager.js';
+import Scene from './src/core/scene.js';
 
-export { GameManager, SceneManager };
+export { GameManager, SceneManager, Scene };
