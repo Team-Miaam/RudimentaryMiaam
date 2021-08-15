93f7906f966629997442ed0789a9a33e5ec3d083
Noman5237 <anonyman637@gmail.com>
2021-08-15 18:18:24 +0600

    #16 added camera support

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ import SceneManager from './src/manager/sceneManager.js';
 import Scene from './src/core/scene.js';
 import Entity from './src/core/entity.js';
 import View from './src/view/view.js';
+import AnimatedSpriteWState from './src/core/animation.js';
+import Camera from './src/core/camera.js';
 
-export { GameManager, SceneManager, Scene, View, Entity };
+export { GameManager, SceneManager, Scene, View, Entity, AnimatedSpriteWState, Camera };
