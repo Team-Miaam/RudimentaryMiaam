95833fb7aed46e25632fba3922eaba329e8768af
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2021-10-04 20:51:39 +0600

    Documentation of keyboard class

--- src/input/keyboard/keyboard.js
+++ src/input/keyboard/keyboard.js
@@ -1,8 +1,19 @@
 import Key from './Key.js';
-
+/**
+ * @class 
+ * Connect between event window and keyboard inputs. 
+ */
 class Keyboard {
+	/**
+	 * @type {Object}
+	 * List of the all the key used in current running game.
+	 */
 	static #keys = {};
 
+	/**
+	 * Checks whether input key is already in Key object,if not bind keyboard with that event window.
+	 * @param {Object} object 
+	 */
 	static bindEventListener(object) {
 		object.addEventListener(
 			'keyup',
@@ -25,7 +36,10 @@ class Keyboard {
 	}
 
 	// FIXME: Buggy behaviour on multiple key presses at same time
-
+	/**
+	 * 
+	 * @param {String} key - checks if key is pressed when key is released.
+	 */
 	static #onKeyUp(key) {
 		if (key.isDown) {
 			key.resolveActionsOnUp();
@@ -33,7 +47,10 @@ class Keyboard {
 		key.isDown = false;
 		key.isUp = true;
 	}
-
+	/**
+	 * 
+	 * @param {String} key - checks if key is pressed when key is pressed.
+	 */
 	static #onKeyDown(key) {
 		if (key.isUp) {
 			key.resolveActionsOnDown();
@@ -41,13 +58,21 @@ class Keyboard {
 		key.isDown = true;
 		key.isUp = false;
 	}
-
+	
+	/**
+	 * 
+	 * @param {String} key - checks and register if key is pressed when key is undefined.
+	 */
 	static #registerKeyIfUndefined(key) {
 		if (!this.#keys[key]) {
 			this.#keys[key] = new Key(key);
 		}
 	}
-
+	/**
+	 * 
+	 * @param {String} key - checks and register if key is pressed when pre-declared key is undefined.
+	 * @returns 
+	 */
 	static key(key) {
 		this.#registerKeyIfUndefined(key);
 		return this.#keys[key];
